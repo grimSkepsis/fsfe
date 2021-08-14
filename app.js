7668b006002344f16a57b8d7454ee2a170237901
grimSkepsis <developer.mikehofer@gmail.com>
2021-08-14 08:36:07 -0700

    Adds new demo route

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ const port = 3000;
 app.get('/', (req, res) => {
 	res.send('Hello World (and Tendy!)');
 });
+app.get('/demo', (req, res) => {
+	res.set('X-full-stack', 'for-life')
+	res.status(418);
+	res.send('Cool demo');
+});
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}!`));
